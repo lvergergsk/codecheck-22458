eb33b066f40f4a9574c241dd93b2d424de833f5e
YOULYU ZHANG <lvergergsk@outlook.com>
2017-12-18 16:28:06 -0800

    trying input format and codecheck configuration.

--- java/src/main/java/codecheck/Main.java
+++ java/src/main/java/codecheck/Main.java
@@ -1,12 +1,14 @@
 package codecheck;
 
 public class Main {
-	public static void main(String... args) {
+    public static void main(String... args) {
 
-	    // Just to see the format of input.
-	    for (String str:args){
-	        System.out.println(str);
+        // Just to see the format of input.
+        System.out.println("TEST_PRINT----");
+        System.out.println(args.length);
+        for (String str : args) {
+            System.out.println(str);
         }
 
-	}
+    }
 }
